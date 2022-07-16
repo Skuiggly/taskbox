547b1c0e81f00103a9b8d13bf38597814007a056
Skuiggly <piplup107@gmail.com>
2022-07-16 03:54:38 +0200

    feat(TaskList): prettify loading state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,9 +7,25 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     onPinTask,
     onArchiveTask,
   };
-
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
   if (loading) {
-    return <div className="list-items">loading</div>;
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
   if (tasks.length === 0) {
     return (
