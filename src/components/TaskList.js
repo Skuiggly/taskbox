47683253c72e44317ee2123d9eddd5134976f121
Skuiggly <piplup107@gmail.com>
2022-07-16 03:53:51 +0200

    feat(TaskList): add pinned task priority

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -11,14 +11,17 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   if (loading) {
     return <div className="list-items">loading</div>;
   }
-
   if (tasks.length === 0) {
     return <div className="list-items">empty</div>;
   }
 
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
   return (
     <div className="list-items">
-      {tasks.map(task => (
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
