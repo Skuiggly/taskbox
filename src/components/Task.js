d2e9aff84d9bf11b8a17e14fac0e9fba31a41ab3
Skuiggly <piplup107@gmail.com>
2022-07-17 15:53:06 +0200

    fix(Task): typo

--- src/components/Task.js
+++ src/components/Task.js
@@ -29,7 +29,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ backround: 'red' }}
+          style={{ background: 'red' }}
         />
       </label>
 
